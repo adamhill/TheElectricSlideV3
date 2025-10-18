6d2c4e0dfc758f7baedbfdea31ac82ee95bf09d4
Adam Hill 🦿 <adam.hill@gmail.com>
2025-10-18 13:28:05 -0500

    Initial Commit

---
+++ TheElectricSlideTests/TheElectricSlideTests.swift
@@ -0,0 +1,16 @@
+//
+//  TheElectricSlideTests.swift
+//  TheElectricSlideTests
+//
+//  Created by Adam Hill on 10/18/25.
+//
+
+import Testing
+
+struct TheElectricSlideTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
